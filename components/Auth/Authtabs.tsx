07083f48402f2f3cd31632d1a7f6fc7b94aee1db
ange <gmail.com>
2024-08-15 15:29:33 +0800

    修改原生button大小

--- components/Auth/Authtabs.tsx
+++ components/Auth/Authtabs.tsx
@@ -1,8 +1,6 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import LoginForm from "./LoginForm";
 import RegisterForm from "./RegisterForm";
-import Link from "next/link";
-import { Button } from "../ui/button";
 
 const AuthTabs = () => {
   return (
