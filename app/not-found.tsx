6e02d83306691d87324bd16bd0f1889704380789
ange <gmail.com>
2024-09-06 19:31:01 +0800

    修正404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import LandingNav from "@/components/LandingPage/LandingNav";
 import NotFoundPage from "@/components/Others/NotFounPage";
 import LandingFooter from "@/components/LandingPage/LandingFooter";
